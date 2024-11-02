74cb3168eeddb5e44da5d8f3874926129fcbaeb3
Roey Zalta <roey.zalta@gmail.com>
2024-11-02 15:45:39 +0200

    added herader css to align logo and search bar

--- ecommerece/src/app/header/page.js
+++ ecommerece/src/app/header/page.js
@@ -1,4 +1,11 @@
 import React from "react"
+import { IoSearch } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import './header.css'
+
+
+
 
 const Header = () => {
     return (
@@ -7,13 +14,27 @@ const Header = () => {
             <div className='top_header'>
                 <div className='logo'>
                     <img src='images/logo.svg' width={100} height={100} /> 
-            </div>
-            <div className='search_box'>
-                <div className='search_bar'>
-                    <input type='text' placeholder='Search for products' />
-                    <button>Search</button>
                 </div>
-            </div>
+                <div className='search_box'>
+                    <div className='search_bar'>
+                        <input type='text' placeholder='Search for products' />
+                        <button>
+                            <IoSearch />
+                        </button>
+                    </div>
+                </div>
+                <div className='function_link'>
+                    <div className='user'>
+                        <div className='icon'>
+                            <FaRegUser />
+                        </div>
+                    </div>
+                    <div className='cart'>
+                        <div className='icon'>
+                            <FaShoppingCart />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         </>
